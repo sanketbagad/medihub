13529ff78e009d1ab4e3c84cd687025f0d36df54
Sanket Bagad <alencolins@gmail.com>
2024-05-23 01:48:41 +0530

    Init

---
+++ src/views/home/index.jsx
@@ -0,0 +1,106 @@
+import { ArrowRightOutlined } from "@ant-design/icons";
+import { MessageDisplay } from "@/components/common";
+import { ProductShowcaseGrid } from "@/components/product";
+import {
+  FEATURED_PRODUCTS,
+  RECOMMENDED_PRODUCTS,
+  SHOP,
+} from "@/constants/routes";
+import {
+  useDocumentTitle,
+  useFeaturedProducts,
+  useRecommendedProducts,
+  useScrollTop,
+} from "@/hooks";
+import bannerImg from "@/images/banner-girl.png";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  useDocumentTitle("Salinaka | Home");
+  useScrollTop();
+
+  const {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured,
+  } = useFeaturedProducts(6);
+  const {
+    recommendedProducts,
+    fetchRecommendedProducts,
+    isLoading: isLoadingRecommended,
+    error: errorRecommended,
+  } = useRecommendedProducts(6);
+
+  return (
+    <main className="content">
+      <div className="home">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1 className="text-thin">
+              <strong>Care </strong>
+              &nbsp;in Every Click&nbsp;
+              <strong>- Your Go-To Online Medical Store.</strong>
+            </h1>
+            <p>
+              MediHub: Your trusted online pharmacy offering a wide range of
+              prescription medications, healthcare products, and wellness
+              essentials. With fast delivery, secure payments, and professional
+              guidance, we’re committed to keeping you healthy and happy.
+              Explore our virtual medicine counter today!” Feel free to
+              customize this description or let me know if you need any further
+              adjustments. 😊
+            </p>
+            <br />
+            <Link to={SHOP} className="button">
+              Shop Now &nbsp;
+              <ArrowRightOutlined />
+            </Link>
+          </div>
+          <div className="banner-img">
+            <img src={bannerImg} alt="" />
+          </div>
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Featured Products</h1>
+            <Link to={FEATURED_PRODUCTS}>See All</Link>
+          </div>
+          {errorFeatured && !isLoadingFeatured ? (
+            <MessageDisplay
+              message={errorFeatured}
+              action={fetchFeaturedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={featuredProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Recommended Products</h1>
+            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+          </div>
+          {errorRecommended && !isLoadingRecommended ? (
+            <MessageDisplay
+              message={errorRecommended}
+              action={fetchRecommendedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={recommendedProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
